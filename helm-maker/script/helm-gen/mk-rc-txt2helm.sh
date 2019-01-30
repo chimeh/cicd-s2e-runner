dac6b1ed9b901ea64138164271d440e3ac08371f
jimin.huang <jimin.huang@nx-engine.com>
2019-01-30 17:13:23 +0800

    实现K8S 一个NS的 img, 服务名, env导出成目录结构

--- helm-maker/script/helm-gen/mk-rc-txt2helm.sh
+++ helm-maker/script/helm-gen/mk-rc-txt2helm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #author huangjimin
 #jimin.huang@nx-engine.com
-#get info from k8s namespace, then generate helm template for all services in the NS
+#convert txtdir to helm 
+echo "usage: $0  txtdir [NEWNAME] "
 
 ###################################################################
-THIS_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0})
+THIS_SCRIPT=$(realpath $(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0}))
 #automatic detection TOPDIR
 CUR_DIR=$(dirname $(realpath ${THIS_SCRIPT}))
 TRYTOP=$(xdir=${CUR_DIR};cd ${CUR_DIR}; while /usr/bin/test ! -e .TOP ; do \
@@ -24,36 +25,49 @@ echo THIS_SCRIPT=$THIS_SCRIPT
 echo CUR_DIR=$CUR_DIR
 echo TRYTOP=$TRYTOP
 
+if [[ -n ${TEAMCITY_GIT_PATH} ]];then
+    echo "Run on Teamcity"
+    BUILD_COUNTER="t${BUILD_NUMBER}"
+elif [[ -n ${JENKINS_URL} ]];then
+    echo "Run on Jenkins CI"
+    BUILD_COUNTER="j${BUILD_NUMBER}"
+else
+    echo "personal rc"
+    BUILD_COUNTER="x"
+fi
 
 ####################################################################
+CURDATE=$(date +%Y%m%d%H%M%S)
 if [ $# -lt 1 ];then
-  echo "useage: $0 namespace"
+  echo "useage: $0 txtdir"
   exit 1;
 fi
-projname=release-$1
-CURDATE=$(date +%Y%m%d-%H-%M-%S)
-RCNAME=${TRYTOP}/../${projname}-${CURDATE}
-vaule_filename=values-release-apps.yaml
-requirement_filename=requirements.yaml
-defaultversion=${CURDATE}
-echo "${projname}-${CURDATE}"
-
-kubectl cluster-info
-SRC_NS=$1
-kubectl get ns ${SRC_NS}
-if [ $? -ne 0 ];then
-  echo "NS ${SRC_NS} no exist"
-  exit 2;
+if [ $# -ge 2 ];then
+  CATALOG_NAME=$(echo $2 | tr '[A-Z]' '[a-z]' |tr -csd  "[0-9._][a-z][A-Z]" "")
+else
+  CATALOG_NAME=$(echo $(basename $1) | tr '[A-Z]' '[a-z]' |tr -csd  "[0-9._][a-z][A-Z]" "")
 fi
+
+VERSION=${CURDATE}-${BUILD_COUNTER}
+
+echo "CHART name set to ${CATALOG_NAME} ${VERSION}"
+
+
+TXTDIR=$(realpath ${1})
+RCNAME=${TRYTOP}/../${CATALOG_NAME}-${VERSION}
+vaule_filename=values-release-txt2helm.yaml
+commonchartversion=1.0
+echo "${CATALOG_NAME}-${VERSION}"
+
+
 mkdir -p ${RCNAME}/charts
 cat >> ${RCNAME}/Chart.yaml <<EOF
-name: ${projname}
-version: ${CURDATE}
+name: ${CATALOG_NAME}
+version: ${VERSION}
 appVersion: 0.1
-description: all icev3-dependcies
+description: gen helm from  $(basename $1)
 keywords:
-- nextengine
-- icev3-dependcies
+- $(basename $1)
 home: https://www.nx-engine.com/
 icon: https://bitnami.com/assets/stacks/postgresql/img/postgresql-stack-110x117.png
 sources:
@@ -63,18 +77,19 @@ maintainers:
   email: jimagile@gmail.com
 engine: gotpl
 EOF
-MWARE="redis|kafka|solr|elasticsearch|hbase|mongo|mysql|strimzi-cluster-operator|pvc|zookeeper"
 
 echo "##########################gen charts, and depencies"
-echo  'dependencies:' > ${RCNAME}/requirements.yaml
-for i in `/bin/ls ice-bus-dev` ; do 
+for i in `/bin/ls ${TXTDIR}`;do \
     name=$i
-    echo "auto gen charts for ${SRC_NS}/${name}"
-    img="harbor-chengdu.nx-engine.com/ice/ne-evgb-dashboard:1.0.2-afe185d"
+    echo "gen charts for /${name}"
+    img=`head -n 1 ${TXTDIR}/${name}/img.txt`
     /bin/cp -rf  ${TRYTOP}/generic/xxx-generic-chart ${RCNAME}/charts/$name
-    echo '' >${RCNAME}/charts/$name/files/env.txt
+    if [[ -f ${TXTDIR}/${name}/env.txt ]];then
+       echo ${TXTDIR}/${name}/env.txt
+        perl -ne "print ' ' x 1;print '#';print \$_" ${TXTDIR}/${name}/env.txt >> ${RCNAME}/charts/$name/files/env.txt
+    fi
     perl -ni -e "s/^name:.+/name: ${name}/g;print" ${RCNAME}/charts/$name/Chart.yaml
-    perl -ni -e "s/^version:.+/version: ${defaultversion}/g;print" ${RCNAME}/charts/$name/Chart.yaml
+    perl -ni -e "s/^version:.+/version: ${commonchartversion}/g;print" ${RCNAME}/charts/$name/Chart.yaml
     perl -ni -e "s/^icev3-xxx-generic/$name/g;print" ${RCNAME}/charts/$name/values-single.yaml
     perl -ni -e "s@image:.+@image: $img@g;print"  ${RCNAME}/charts/$name/values-single.yaml
     echo "dependcies"
@@ -95,14 +110,17 @@ global:
       domain: okd.cd
     public:
       annotations-ingress-class: kong-ingress-public
-      domain: nx-ice.com
+      domain: nx-code.com
+  containers:
+    securityContext:
+      privileged: true
 EOF
 
-MWARE="redis|kafka|solr|elasticsearch|hbase|mongo|mysql|strimzi-cluster-operator|pvc|zookeeper"
 
-for i in `/bin/ls ice-bus-dev` ; do 
+for i in `/bin/ls ${TXTDIR}`;do \
     name=$i
-    img=nginx
+    echo "gen charts for /${name}"
+    img=`head -n 1 ${TXTDIR}/${name}/img.txt`
 cat >> ${RCNAME}/${vaule_filename} <<EOF
 ${name}:
   ${name}:
@@ -122,62 +140,23 @@ ${name}:
     env.txt: |
       #from ${vaule_filename}
 EOF
+if [[ -f ${TXTDIR}/${name}/env.txt ]];then
+   echo ${TXTDIR}/${name}/env.txt
+    perl -ne "print ' ' x 6;print \$_" ${TXTDIR}/${name}/env.txt >> ${RCNAME}/${vaule_filename}
+fi
 done
 
 echo "##############################################################"
-echo "##########################gen middleware charts, and depencies"
-MWDIR=${RCNAME}/charts/infra-middleware/charts
-MW_VALUEFILE=${RCNAME}/values-middleware-all-in-one.yaml
-
-mkdir -p ${MWDIR}
-/bin/cp -rf  ${TRYTOP}/infra-middleware/* ${MWDIR}
-
-cat  >>  ${MW_VALUEFILE} <<EOF
-infra-middleware:
-EOF
-#################merge mw chart into one
-echo  'dependencies:' > $(dirname ${MWDIR})/requirements.yaml
-for i in `/bin/ls ${MWDIR} `; do 
-    name=$i
-    echo "auto gen charts for middleware ${name}"
-    version=$( cat ${MWDIR}/${name}/Chart.yaml   |egrep "^version" | perl -ne 'print $1 if /^version:[ ]+(.+)/')
-    mv ${MWDIR}/${name}/values.yaml ${MWDIR}/${name}/values-single.yaml
-
-echo " let infra-middleware depend ${name}"
-cat >> $(dirname ${MWDIR})/requirements.yaml <<EOF
-- name: ${name}
-  version: ~${version}
-  repository: "file://charts/${name}"
-  condition: ${name}.enabled
-EOF
-echo " merge ${name} valuefile"
-cat  >>  ${MW_VALUEFILE} <<EOF
-  ${name}:
-    enabled: false
-EOF
-perl  -ne 'print "    $_"' ${MWDIR}/${name}/values-single.yaml  >>  ${MW_VALUEFILE}
-done 
 
 
-
-##################let mw below top chart
-echo "name: infra-middleware" > $(dirname ${MWDIR})/Chart.yaml
-echo "version: 0.9.1" >> $(dirname ${MWDIR})/Chart.yaml
-
-cat >> ${RCNAME}/requirements.yaml <<EOF
-- name: infra-middleware
-  version: ~0.9.1
-  repository: "file://charts/infra-middleware"
-EOF
 cat ${RCNAME}/${vaule_filename} > ${RCNAME}/values.yaml
-cat ${MW_VALUEFILE} >> ${RCNAME}/values.yaml
 
 ################# post to repo
 if [ $# -gt 1 ];then
   echo "post to repo"
-  rm -rf ${RCNAME}/../${projname}
-  /bin/cp -rf ${RCNAME} ${RCNAME}/../${projname}
+  rm -rf ${RCNAME}/../${CATALOG_NAME}
+  /bin/cp -rf ${RCNAME} ${RCNAME}/../${CATALOG_NAME}
   cd ${RCNAME}/..
-  helm package ${projname}
-  curl --data-binary "@${projname}-${CURDATE}.tgz" http://charts.ops/api/charts
+  helm package ${CATALOG_NAME}
+  curl --data-binary "@${CATALOG_NAME}-${VERSION}.tgz" http://charts.ops/api/charts
 fi
---
+++ helm-maker/script/k8s-exporter/k8s-ns-apps-export.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#author huangjimin
+#jimin.huang@nx-engine.com
+#get info from k8s namespace, then generate helm template for all services in the NS
+
+###################################################################
+THIS_SCRIPT=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0})
+#automatic detection TOPDIR
+CUR_DIR=$(dirname $(realpath ${THIS_SCRIPT}))
+TRYTOP=$(xdir=${CUR_DIR};cd ${CUR_DIR}; while /usr/bin/test ! -e .TOP ; do \
+        xdir=`cd ../;pwd`;                       \
+        if [ "$xdir" = "/" ] ; then             \
+           echo  1>&2; \
+           break;                              \
+        fi ;                                    \
+        cd $xdir;                               \
+        done ;                                  \
+        pwd;)
+
+if [[ -z ${TRYTOP} ]];then
+TRYTOP=${CUR_DIR}
+fi
+echo THIS_SCRIPT=$THIS_SCRIPT
+echo CUR_DIR=$CUR_DIR
+echo TRYTOP=$TRYTOP
+
+if [[ -n ${TEAMCITY_GIT_PATH} ]];then
+    echo "Run on Teamcity"
+    BUILD_COUNTER="t${BUILD_NUMBER}"
+elif [[ -n ${JENKINS_URL} ]];then
+    echo "Run on Jenkins CI"
+    BUILD_COUNTER="j${BUILD_NUMBER}"
+else
+    echo "personal rc"
+    BUILD_COUNTER="x"
+fi
+
+
+####################################################################
+
+if [ $# -lt 1 ];then
+  echo "useage: $0 namespace"
+  exit 1;
+fi
+SRC_NS=$1
+CURDATE=$(date +%Y%m%d%H%M%S)
+VERSION=${CURDATE}-${BUILD_COUNTER}
+echo "NS ${SRC_NS} ${VERSION}"
+
+RCNAME=${TRYTOP}/../${SRC_NS}-${VERSION}
+echo "${SRC_NS}-${CURDATE}"
+echo "${RCNAME}"
+
+kubectl cluster-info
+kubectl get ns ${SRC_NS}
+if [ $? -ne 0 ];then
+  echo "NS ${SRC_NS} no exist"
+  exit 2;
+fi
+mkdir -p ${RCNAME}
+
+MWARE="redis|kafka|solr|elasticsearch|hbase|mongo|mysql|strimzi-cluster-operator|pvc|zookeeper"
+echo "##########################gen img/ env/ port"
+kubectl get -n ${SRC_NS} deployment  --no-headers |  awk '{print $1}' | egrep -v "${MWARE}" | \
+while read i; do 
+    name=$i
+    echo "export app for ${SRC_NS}/${name}"
+    mkdir -p ${RCNAME}/${name}
+    img=`kubectl get -n ${SRC_NS} deployment $i  -o=jsonpath='{.spec.template.spec.containers[0].image}'`
+    echo ${img} > ${RCNAME}/${name}/img.txt
+    kubectl get -n ${SRC_NS} cm $name -o=jsonpath='{.data.env\.txt}' > ${RCNAME}/${name}/env.txt
+    kubectl get -n ${SRC_NS} cm $name -o=jsonpath='{.data.default-entrypoint\.sh}' 2>/dev/null > ${RCNAME}/${name}/default-entrypoint.sh
+    if [[ $(wc -l ${RCNAME}/${name}/default-entrypoint.sh  | awk '{print $1}') -lt 1 ]];then
+      rm -f  ${RCNAME}/${name}/default-entrypoint.sh
+    fi
+    kubectl get -n ${SRC_NS} cm $name -o=jsonpath='{.data.override-entrypoint\.sh}' 2>/dev/null > ${RCNAME}/${name}/override-entrypoint.sh
+    if [[ $(wc -l ${RCNAME}/${name}/override-entrypoint.sh  | awk '{print $1}') -lt 1 ]];then
+      rm -f  ${RCNAME}/${name}/override-entrypoint.sh
+    fi
+    kubectl get -n ${SRC_NS} --export cm ${name}-initdata -o=yaml >${RCNAME}/${name}/${name}-initdata.yaml 2>/dev/null
+    if [[ $(wc -l ${RCNAME}/${name}/${name}-initdata.yaml  | awk '{print $1}') -lt 1 ]];then
+      rm -f  ${RCNAME}/${name}/${name}-initdata.yaml
+    fi
+done
+
+
