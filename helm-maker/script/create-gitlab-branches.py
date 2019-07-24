70e68529cc01668e4b6755ba686b0a39e6d75a27
宋文武 <wenwu.song@nx-engine.com>
2019-07-24 09:47:50 +0800

    Update some scripts...

--- helm-maker/script/create-gitlab-branches.py
+++ helm-maker/script/create-gitlab-branches.py
@@ -19,7 +19,7 @@ def run(args):
         for cm in v1.list_namespaced_config_map(args.kubernetes_namespace).items:
             with contextlib.suppress(KeyError):
                 meta = {}
-                for line in cm.data['srcmeta.txt'].splitlines():
+                for line in cm.data["srcmeta.txt"].splitlines():
                     key, val = line.partition("=")[::2]
                     meta[key] = val
                 targets.add((meta["CI_PROJECT_ID"], meta["CI_COMMIT_SHA"]))
@@ -34,41 +34,33 @@ def run(args):
         for project_id, ref in targets:
             project = gl.projects.get(project_id)
             try:
-                project.branches.create({
-                    "branch": args.branch_name,
-                    "ref": ref,
-                })
+                project.branches.create({"branch": args.branch_name, "ref": ref})
                 print(f"[DONE] {project.path_with_namespace}")
             except gitlab.GitlabError as e:
                 print(f"[FAIL] {project.path_with_namespace} -> {e}")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='创建 GitLab 项目分支。')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="创建 GitLab 项目分支。")
     parser.add_argument(
-        "--dry-run", action='store_true', default=False,
-        help='输出将要创建分支的项目，不进行分支创建。',
+        "--dry-run", action="store_true", default=False, help="输出将要创建分支的项目，不进行分支创建。"
     )
-    parser.add_argument(
-        "--branch-name", required=True,
-        help='将要创建的分支名。',
-    )
+    parser.add_argument("--branch-name", required=True, help="将要创建的分支名。")
     parser.add_argument(
         "--gitlab-url",
         default=os.environ.get("GITLAB_URL", "https://git.nx-code.com"),
-        help='GitLab URL',
+        help="GitLab URL",
     )
     parser.add_argument(
         "--gitlab-private-token",
         default=os.environ.get("GITLAB_PRIVATE_TOKEN", ""),
-        help='GitLab private access token',
+        help="GitLab private access token",
     )
     parser.add_argument(
-        "--no-kubernetes", action='store_true', default=False,
-        help='不从 kubernetes 获取项目列表。',
+        "--no-kubernetes",
+        action="store_true",
+        default=False,
+        help="不从 kubernetes 获取项目列表。",
     )
-    parser.add_argument(
-        "--kubernetes-namespace", required=True,
-        help='指定 namespace 。',
-    )
+    parser.add_argument("--kubernetes-namespace", required=True, help="指定 namespace 。")
     run(parser.parse_args())
--- helm-maker/script/download-gitlab-codes.py
+++ helm-maker/script/download-gitlab-codes.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import contextlib
 import tarfile
+from tempfile import TemporaryDirectory
 
 import gitlab
 
@@ -14,7 +15,7 @@ def run(args):
     print("正在获取项目列表。。。")
 
     targets = []
-    for project in gl.projects.list(as_list=False):
+    for project in (gl.projects.list())[:3]:
         with contextlib.suppress(gitlab.GitlabError):
             project.branches.get(args.ref)
             targets.append(project)
@@ -27,31 +28,40 @@ def run(args):
         print("将获取以下项目的源代码：")
         for project in targets:
             print(f"  {project.path_with_namespace}")
+    else:
+        with TemporaryDirectory() as t:
+            print(f"开始下载项目源代码，总计：{len(targets)}")
+            for i, project in enumerate(targets):
+                print(f"[{i+1}/{len(targets)}] 下载 {project.path_with_namespace}")
+                os.makedirs(
+                    os.path.join(t, os.path.dirname(project.path_with_namespace))
+                )
+                with open(
+                    os.path.join(t, project.path_with_namespace + ".tar.gz"), mode="wb"
+                ) as f:
+                    project.repository_archive(
+                        sha=args.ref, streamed=True, action=f.write
+                    )
+
+            with tarfile.open(args.output, mode="w|gz") as tar:
+                tar.add(t, arcname="source-codes")
 
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='下载 GitLab 项目源代码。')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="下载 GitLab 项目源代码。")
     parser.add_argument(
-        "--dry-run", action='store_true', default=False,
-        help='输出将要下载代码的项目，不进行代码下载。',
+        "--dry-run", action="store_true", default=False, help="输出将要下载代码的项目，不进行代码下载。"
     )
-    parser.add_argument(
-        "--ref", required=True,
-        help="将要下载的分支名或者TAG名。"
-    )
+    parser.add_argument("--ref", required=True, help="将要下载的分支名或者TAG名。")
     parser.add_argument(
         "--gitlab-url",
         default=os.environ.get("GITLAB_URL", "https://git.nx-code.com"),
-        help='GitLab URL',
+        help="GitLab URL",
     )
     parser.add_argument(
         "--gitlab-private-token",
         default=os.environ.get("GITLAB_PRIVATE_TOKEN", ""),
-        help='GitLab private access token',
+        help="GitLab private access token",
     )
-    parser.add_argument(
-        "--output",
-        help="输出文件路径",
-    )
+    parser.add_argument("--output", required=True, help="输出文件路径")
     run(parser.parse_args())
