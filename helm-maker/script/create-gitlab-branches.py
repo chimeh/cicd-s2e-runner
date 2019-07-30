c60be54b33081b18e780a625ed9473b76ddac0b8
宋文武 <wenwu.song@nx-engine.com>
2019-07-30 15:41:47 +0800

    create-gitlab-branches.py: 支持 txtdir，支持创建 tag 。

--- helm-maker/script/create-gitlab-branches.py
+++ helm-maker/script/create-gitlab-branches.py
@@ -24,20 +24,47 @@ def run(args):
                     meta[key] = val
                 targets.add((meta["CI_PROJECT_ID"], meta["CI_COMMIT_SHA"]))
 
-    if args.dry_run:
-        print(f"将在以下项目创建 {args.branch_name} 分支：")
-        for project_id, _ in targets:
-            project = gl.projects.get(project_id)
-            print(f"  {project.path_with_namespace}")
+    if args.txtdir:
+        for dirpath, dirnames, filenames in os.walk(args.txtdir):
+            if "srcmeta.txt" in filenames:
+                with contextlib.suppress(KeyError):
+                    meta = {}
+                    for line in open(os.path.join(dirpath, "srcmeta.txt")):
+                        key, val = line.strip().split("=")
+                        meta[key] = val
+                    targets.add((meta["CI_PROJECT_ID"], meta["CI_COMMIT_SHA"]))
+
+    if not args.create_tag:
+        if args.dry_run:
+            print(f"将在以下项目创建 {args.branch_name} 分支：")
+            for project_id, _ in targets:
+                project = gl.projects.get(project_id)
+                print(f"  {project.path_with_namespace}")
+        else:
+            print(f"创建 {args.branch_name} 分支：")
+            for project_id, ref in targets:
+                project = gl.projects.get(project_id)
+                try:
+                    project.branches.create({"branch": args.branch_name, "ref": ref})
+                    print(f"[DONE] {project.path_with_namespace}")
+                except gitlab.GitlabError as e:
+                    print(f"[FAIL] {project.path_with_namespace} -> {e}")
+
     else:
-        print(f"创建 {args.branch_name} 分支：")
-        for project_id, ref in targets:
-            project = gl.projects.get(project_id)
-            try:
-                project.branches.create({"branch": args.branch_name, "ref": ref})
-                print(f"[DONE] {project.path_with_namespace}")
-            except gitlab.GitlabError as e:
-                print(f"[FAIL] {project.path_with_namespace} -> {e}")
+        if args.dry_run:
+            print(f"将在以下项目创建 {args.branch_name} tag：")
+            for project_id, _ in targets:
+                project = gl.projects.get(project_id)
+                print(f"  {project.path_with_namespace}")
+        else:
+            print(f"创建 {args.branch_name} tag：")
+            for project_id, ref in targets:
+                project = gl.projects.get(project_id)
+                try:
+                    project.tags.create({"tag_name": args.branch_name, "ref": ref})
+                    print(f"[DONE] {project.path_with_namespace}")
+                except gitlab.GitlabError as e:
+                    print(f"[FAIL] {project.path_with_namespace} -> {e}")
 
 
 if __name__ == "__main__":
@@ -62,5 +89,11 @@ if __name__ == "__main__":
         default=False,
         help="不从 kubernetes 获取项目列表。",
     )
-    parser.add_argument("--kubernetes-namespace", required=True, help="指定 namespace 。")
+    parser.add_argument(
+        "--kubernetes-namespace", default="default", help="指定 namespace 。"
+    )
+    parser.add_argument("--txtdir", default=None, help="从 txtdir 读取项目列表。")
+    parser.add_argument(
+        "--create-tag", default=False, action="store_true", help="创建 tag 而不是分支。"
+    )
     run(parser.parse_args())
