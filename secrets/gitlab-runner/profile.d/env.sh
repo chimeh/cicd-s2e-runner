0b5759a9f9f8519b609d8c0815cee81d83a0ae9e
Jimmy <jimin.huang@nx-engine.com>
2020-03-02 13:18:27 +0800

    docker-entrypoint.sh

--- secrets/gitlab-runner/profile.d/env.sh
+++ secrets/gitlab-runner/profile.d/env.sh
@@ -9,8 +9,8 @@ export DOCKER_REPO=docker.io
 export DOCKER_NS=bettercode
 export K8S_AUTOCD=0
 export K8S_NS=default
-export K8S_DOMAIN_INTERNAL=k8s-dev.cd
-export K8S_DOMAIN_PUBLIC=cd-dev.nxengine.com
+export K8S_DOMAIN_INTERNAL=bu5-dev.cd
+export K8S_DOMAIN_PUBLIC=bu5-dev.nxengine.com
 export ENABLE_SONAR=0
 
 
@@ -22,6 +22,3 @@ export K8S_KUBECONFIG_DEV=/root/.kube/dev.config
 export K8S_KUBECONFIG_TEST=/root/.kube/test.config
 export K8S_KUBECONFIG_PROD=/root/.kube/prod.config
 
-export LD_LIBRARY_PATH=/lib:/lib64:/usr/lib
-export ANDROID_SDK_ROOT=/usr/local/android
-export PATH="$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools:$PATH"
