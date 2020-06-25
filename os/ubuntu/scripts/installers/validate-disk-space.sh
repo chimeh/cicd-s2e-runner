92b17b0172dc190805b4ca0886e26da1f1e421ee
Jimmy <jimin.huang@benload.com>
2020-06-25 16:34:13 +0800

    Dockerfile.ubuntu

--- os/ubuntu/scripts/installers/validate-disk-space.sh
+++ os/ubuntu/scripts/installers/validate-disk-space.sh
@@ -6,6 +6,7 @@
 
 availableSpaceMB=$(df / -hm | sed 1d | awk '{ print $4}')
 minimumFreeSpaceMB=16000
+gcminimumFreeSpaceMB=20000
 
 echo "Available disk space: $availableSpaceMB MB"
 
@@ -13,7 +14,12 @@ if [ $RUN_VALIDATION != "true" ]; then
     echo "Skipping validation disk space..."
     exit 0
 fi
-
+if [ $availableSpaceMB -le $gcminimumFreeSpaceMB ]; then
+  THIS_SCRIPT=$(realpath $(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0}))
+  #automatic detection TOPDIR
+  SCRIPT_DIR=$(dirname $(realpath ${THIS_SCRIPT}))
+  bash ${SCRIPT_DIR}/cleanup.sh
+fi
 if [ $availableSpaceMB -le $minimumFreeSpaceMB ]; then
     echo "Not enough disk space on the image (minimum available space: $minimumFreeSpaceMB MB)"
     exit 1
