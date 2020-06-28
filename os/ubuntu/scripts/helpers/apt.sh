9058de65401ee2debb343b9b52428e630804409a
Jimmy <jimin.huang@benload.com>
2020-06-28 14:34:41 +0800

    自动检测是否处于腾讯云VPC，是则试用VPC内网mirror加速

--- os/ubuntu/scripts/helpers/apt.sh
+++ os/ubuntu/scripts/helpers/apt.sh
@@ -13,8 +13,20 @@ function IsInstalled {
     dpkg -S $1 &> /dev/null
 }
 
+cp /etc/apt/sources.list /etc/apt/sources.list.origin
 # Configure apt to always assume Y
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
+
+set +e
+curl --connect-timeout 1 http://metadata.tencentyun.com/latest/meta-data/instance-id
+rv=$?
+set -e
+if [ ${rv} -eq 0 ];then
+sed -i 's|^\(\s*deb\s*\)http://security.ubuntu.com/ubuntu/|\1http://mirrors.tencentyun.com/ubuntu|' /etc/apt/sources.list
+sed -i 's|^\(\s*deb\s*\)http://archive.ubuntu.com/ubuntu/|\1http://mirrors.tencentyun.com/ubuntu|' /etc/apt/sources.list
+  return
+fi
+
 
 # Use apt-fast for parallel downloads
 apt-get install aria2
