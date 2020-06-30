f9ee83b8c15bbd7ec5f95e40eef83dbcaa49599f
Jimmy <jimin.huang@benload.com>
2020-06-30 13:43:55 +0800

    Dockerfile.ubuntu

--- os/ubuntu/scripts/helpers/basic.sh
+++ os/ubuntu/scripts/helpers/basic.sh
@@ -121,3 +121,62 @@ for package in ${pkg_list[*]}; do
 done
 
 DocumentInstalledItemIndent "$libcurelVer"
+
+
+
+##################################################git
+
+## Install git
+add-apt-repository ppa:git-core/ppa -y
+apt-get update
+apt-get install git -y
+git --version
+
+# Install git-lfs
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y --no-install-recommends git-lfs
+
+# Install git-ftp
+apt-get install git-ftp -y
+
+# Run tests to determine that the software installed as expected
+echo "Testing git installation"
+if ! command -v git; then
+    echo "git was not installed"
+    exit 1
+fi
+echo "Testing git-lfs installation"
+if ! command -v git-lfs; then
+    echo "git-lfs was not installed"
+    exit 1
+fi
+echo "Testing git-ftp installation"
+if ! command -v git-ftp; then
+    echo "git-ftp was not installed"
+    exit 1
+fi
+
+# Document what was added to the image
+echo "Lastly, document the installed versions"
+DocumentInstalledItem "Git/SVN:"
+# git version 2.20.1
+DocumentInstalledItemIndent "git ($(git --version 2>&1 | cut -d ' ' -f 3))"
+# git-lfs/2.6.1 (GitHub; linux amd64; go 1.11.1)
+DocumentInstalledItemIndent "git lfs, Git Large File Storage (LFS) ($(git-lfs --version 2>&1 | cut -d ' ' -f 1 | cut -d '/' -f 2))"
+DocumentInstalledItemIndent "git ftp, ($(git-ftp --version | cut -d ' ' -f 3))"
+
+##################################################svn
+# Install Subversion
+apt-get install -y --no-install-recommends subversion
+
+# Run tests to determine that the software installed as expected
+echo "Testing to make sure that script performed as expected, and basic scenarios work"
+if ! command -v svn; then
+    echo "Subversion (svn) was not installed"
+    exit 1
+fi
+
+# Document what was added to the image
+echo "Lastly, documenting what we added to the metadata file"
+DocumentInstalledItemIndent "svn, Subversion ($(svn --version | head -n 1))"
+
