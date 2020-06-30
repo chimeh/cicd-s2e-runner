cf1047cb90271a3a9de5cec2cd1fd53f5566d671
Jimmy <jimin.huang@benload.com>
2020-06-30 15:03:33 +0800

    Dockerfile.ubuntu

---
+++ os/ubuntu/scripts/installers/cloud-huawei-cli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+################################################################################
+##  File:  aliyun-cli.sh
+##  Desc:  Installs Huawei Cloud CLI
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/document.sh
+source $HELPER_SCRIPTS/apt.sh
+
+# Install Alibaba Cloud CLI
+URL=https://obs-community.obs.cn-north-1.myhuaweicloud.com/obsutil/current/obsutil_linux_amd64.tar.gz
+wget -P /tmp $URL
+tar xzvf /tmp/obsutil_linux_amd64.tar.gz
+mv /tmp/obsutil_linux_amd64_*/obsutil /usr/local/bin/
+
+# Run tests to determine that the software installed as expected
+echo "Testing to make sure that script performed as expected, and basic scenarios work"
+if ! command -v obsutil ; then
+    echo "aliyun was not installed"
+    exit 1
+fi
+
+# Document what was added to the image
+obsutil_version="$(obsutil version | head -n 1)"
+echo "Lastly, documenting what we added to the metadata file"
+DocumentInstalledItem "obsutil, Huawei Cloud obs cli ($obsutil_version)"
