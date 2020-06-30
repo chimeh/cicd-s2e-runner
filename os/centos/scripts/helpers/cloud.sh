8c421f71278ccc9607c8406a9e25a8b57520cf3f
huangjimin <jimminh@163.com>
2020-06-30 15:47:49 +0800

    centos yum mirror

--- os/centos/scripts/helpers/cloud.sh
+++ os/centos/scripts/helpers/cloud.sh
@@ -4,27 +4,35 @@
 ##  Desc:  auto detect run on which cloud
 ################################################################################
 
+if ! command -v scurl; then
+  if  command -v curl; then
+    ln -s $(which curl) /usr/local/bin/scurl
+  fi
+fi > /dev/null
+
 # detect cloudinit
-fuction runon_tencentcloud(){
+function runon_tencentcloud()
+{
   set +e
   scurl --connect-timeout 1 http://metadata.tencentyun.com/latest/meta-data/instance-id >/dev/null 2>&1
   rv=$?
   set -e
   if [[ ${rv} -eq 0 ]];then
-    return true
+    true 
   else
-    return false
+    false
   fi
 }
 
-fuction runon_aliyun(){
+function runon_aliyun ()
+{
   set +e
   scurl --connect-timeout 1 curl http://100.100.100.200 >/dev/null 2>&1
   rv=$?
   set -e
   if [[ ${rv} -eq 0 ]];then
-    return true
+    true
   else
-    return false
+    false
   fi
 }
