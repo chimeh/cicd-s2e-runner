9d63d06f2bc4a596ae74a3e7c2f2e8352cdde13e
huangjimin <jimminh@163.com>
2020-07-01 19:47:18 +0800

    centos base runner

--- os/centos/scripts/helpers/yum.sh
+++ os/centos/scripts/helpers/yum.sh
@@ -7,9 +7,6 @@ source ${SCRIPT_DIR}/cloud.sh
 ls /etc/yum.repos.d
 sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
 sed -i 's/mirrorlist/#mirrorlist/' /etc/yum.repos.d/*.repo
-if runon_tencentcloud;then
-  sed -i 's|#\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.cloud.tencent.com/centos/$releasever|' /etc/yum.repos.d/*.repo
-fi
 
 
 egrep "^baseurl" -r /etc/yum.repos.d
@@ -34,3 +31,5 @@ if runon_aliyun;then
 fi
 
 
+sed -i 's|#\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirror.centos.org/centos/$releasever|' /etc/yum.repos.d/*.repo
+sed -i 's|^#(baseurl=https://download.fedoraproject.org/pub/epel)|\1|' /etc/yum.repos.d/epel*
