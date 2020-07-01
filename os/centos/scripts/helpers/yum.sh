7f7d7c1bba2e3ff9ee91ab0b8c13c76847924aa4
huangjimin <jimminh@163.com>
2020-07-01 20:24:18 +0800

    centos runner

--- os/centos/scripts/helpers/yum.sh
+++ os/centos/scripts/helpers/yum.sh
@@ -11,11 +11,11 @@ sed -i 's/mirrorlist/#mirrorlist/' /etc/yum.repos.d/*.repo
 
 egrep "^baseurl" -r /etc/yum.repos.d
 
-yum install -y --nogpgcheck  epel-release
 
 # Tencent Cloud
 if runon_tencentcloud;then
   sed -i 's|#\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirrors.tencentyun.com/centos/$releasever|' /etc/yum.repos.d/*.repo
+  yum install -y --nogpgcheck  epel-release
   sed -e 's|^metalink=|#metalink=|g' \
      -e 's|^#baseurl=https\?://download.fedoraproject.org/pub/epel/|baseurl=http://mirrors.tencentyun.com/epel/|g' \
      -i /etc/yum.repos.d/epel.repo
@@ -25,11 +25,13 @@ fi
 
 if runon_aliyun;then
   sed -i 's|#\baseurl=https\?://mirror.centos.org/centos/$releasever|baseurl=http://mirrors.cloud.aliyuncs.com/centos/$releasever|' /etc/yum.repos.d/*.repo
+  yum install -y --nogpgcheck  epel-release
   sed -i 's|^#baseurl=https://download.fedoraproject.org/pub|baseurl=https://mirrors.aliyun.com|' /etc/yum.repos.d/epel*
   sed -i 's|^metalink|#metalink|' /etc/yum.repos.d/epel*
   exit 0
 fi
 
 
-sed -i 's|#\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirror.centos.org/centos/$releasever|' /etc/yum.repos.d/*.repo
-sed -i -e 's|^metalink=|#metalink=|g'  -e's|^#\(baseurl.*\)download.fedoraproject.org/pub/epel|\1download.fedoraproject.org/pub/epel|' /etc/yum.repos.d/epel*
+  sed -i 's|#\(baseurl.*\)mirror.centos.org/centos/$releasever|\1mirror.centos.org/centos/$releasever|' /etc/yum.repos.d/*.repo
+  yum install -y --nogpgcheck  epel-release
+  sed -i -e 's|^metalink=|#metalink=|g'  -e's|^#\(baseurl.*\)download.fedoraproject.org/pub/epel|\1download.fedoraproject.org/pub/epel|' /etc/yum.repos.d/epel*
