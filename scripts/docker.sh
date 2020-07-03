a5d4450bf12c865b0025a3e0150e78ef0b23671b
Jimmy <jimin.huang@benload.com>
2020-07-03 17:41:02 +0800

    auto release script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
 THIS_SCRIPT=$(realpath $(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/$(basename ${BASH_SOURCE:-$0}))
 #automatic detection TOPDIR
 SCRIPT_DIR=$(dirname $(realpath ${THIS_SCRIPT}))
@@ -11,7 +13,7 @@ USAGE="
   I. build docker:
   $(basename $(realpath $0)) [/path/to/your-dockerfile] docker-build
   II. build docker then push:
-  export DOCKER_REPO=
+  export DOCKER_HOST=
   export DOCKER_NS=
   export DOCKER_USER=
   export DOCKER_PASS=
@@ -49,11 +51,15 @@ else
     ACTION="docker-build"
 fi
 
-REPO_NAME=$(basename ${SRC_TOP})
-DOCKERFILE_DIR=$(dirname "$(realpath "${DOCKERFILE}")" | tr '[A-Z]' '[a-z]')
-DOCKERFILE_NAME=$(basename ${DOCKERFILE} | tr '[A-Z]' '[a-z]')
-IMG_TMP=$(echo "${REPO_NAME}-${DOCKERFILE_NAME}" | tr '[A-Z]' '[a-z]')
-ARTIFACT_DIR="${SRC_TOP}/dist/${DOCKERFILE_NAME}"
+readonly REPO_NAME=$(basename ${SRC_TOP})
+readonly DOCKERFILE_DIR=$(dirname "$(realpath "${DOCKERFILE}")" | tr '[A-Z]' '[a-z]')
+readonly DOCKERFILE_NAME=$(basename ${DOCKERFILE} | tr '[A-Z]' '[a-z]')
+readonly IMG_TMP=$(echo "${REPO_NAME}-${DOCKERFILE_NAME}" | tr '[A-Z]' '[a-z]')
+readonly ARTIFACT_DIR="${SRC_TOP}/dist/${DOCKERFILE_NAME}"
+
+readonly SRC_VERSION=$(head -n 1 ${SRC_TOP}/VERSION)
+readonly SRC_SHA=-$(git rev-parse --short HEAD)
+readonly OS_DIST=$(echo ${DOCKERFILE_NAME} |cut -d. -f2)
 
 function do_docker_build() {
 
@@ -67,42 +73,67 @@ function do_docker_build() {
 
   echo "> Docker image size: $(($(docker inspect ${IMG_TMP} --format='{{.Size}}')/1000/1000))MB" | tee -a ${ARTIFACT_DIR}/buildnote.md
 
+  echo -e "\n\n" >> ${ARTIFACT_DIR}/buildnote.md
   cat ${ARTIFACT_DIR}/s2erunner/.tpl/*.md >>  ${ARTIFACT_DIR}/buildnote.md
-  echo "" >> ${ARTIFACT_DIR}/buildnote.md
+  echo "\n\n" >> ${ARTIFACT_DIR}/buildnote.md
   set -e
 
   cat ${ARTIFACT_DIR}/buildnote.md
 }
 
+function do_validate_ci_version() {
+  # Accept things like "v1.2.3-alpha.4.56+abcdef12345678" or "v1.2.3-beta.4"
+  local -r version_regex="^v(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)-([a-zA-Z0-9]+)\\.(0|[1-9][0-9]*)(\\.(0|[1-9][0-9]*)\\+[0-9a-f]{7,40})?$"
+  local -r version="${1-}"
+  [[ "${version}" =~ ${version_regex} ]] || {
+    echo "Invalid ci version: '${version}', must match regex ${version_regex}"
+    return 1
+  }
+
+  # The VERSION variables are used when this file is sourced, hence
+  # the shellcheck SC2034 'appears unused' warning is to be ignored.
+
+  # shellcheck disable=SC2034
+  VERSION_MAJOR="${BASH_REMATCH[1]}"
+  # shellcheck disable=SC2034
+  VERSION_MINOR="${BASH_REMATCH[2]}"
+  # shellcheck disable=SC2034
+  VERSION_PATCH="${BASH_REMATCH[3]}"
+  # shellcheck disable=SC2034
+  VERSION_PRERELEASE="${BASH_REMATCH[4]}"
+  # shellcheck disable=SC2034
+  VERSION_PRERELEASE_REV="${BASH_REMATCH[5]}"
+  # shellcheck disable=SC2034
+  VERSION_BUILD_INFO="${BASH_REMATCH[6]}"
+  # shellcheck disable=SC2034
+  VERSION_COMMITS="${BASH_REMATCH[7]}"
+}
+
 function do_docker_push() {
-  DOCKER_REPO=${DOCKER_REPO:-registry-1.docker.io}
-  DOCKER_NS=${DOCKER_NS:-bettercode}
-  DOCKER_USER=${DOCKER_USER:-bettercode}
-  DOCKER_PASS=${DOCKER_PASS}
+  readonly DOCKER_HOST=${DOCKER_HOST:-registry-1.docker.io}
+  readonly DOCKER_NS=${DOCKER_NS:-bettercode}
+  readonly DOCKER_IMG=${REPO_NAME}
+  readonly DOCKER_USER=${DOCKER_USER:-bettercode}
+  readonly DOCKER_PASS=${DOCKER_PASS}
 
   if [[ -n ${DOCKER_PASS} ]];then
-    docker login -u "${DOCKER_USER}" -p  "${DOCKER_PASS}" ${DOCKER_REPO}/${DOCKER_NS}
-    IMAGE_URL=${DOCKER_REPO}/${DOCKER_NS}/$(basename ${PWD})
-    # Change all uppercase to lowercase
-    IMAGE_URL=$(echo $IMAGE_URL | tr '[A-Z]' '[a-z]')
-
-    # Strip git ref prefix from version
-    SRC_VERSION=$(echo "$(git describe  --tags --always|head -n 1)" | sed -e 's,.*/\(.*\),\1,')
-    TAG=${SRC_VERSION}
+    docker login -u "${DOCKER_USER}" -p  "${DOCKER_PASS}" ${DOCKER_HOST}/${DOCKER_NS}
 
 
+    readonly IMAGE_URL=$(echo ${DOCKER_HOST}/${DOCKER_NS}/${DOCKER_IMG}| tr '[A-Z]' '[a-z]')
+    readonly DOCKER_TAG=${SRC_VERSION}-${OS_DIST}-${SRC_SHA}
     echo IMAGE_URL=$IMAGE_URL
-    echo SRC_VERSION=$SRC_VERSION
-    echo TAG=$TAG
+    echo DOCKER_TAG=$DOCKER_TAG
 
-    docker tag ${IMG_TMP} $IMAGE_URL:${TAG}
+    docker tag ${IMG_TMP} $IMAGE_URL:${DOCKER_TAG}
     docker tag ${IMG_TMP} $IMAGE_URL:latest
-    docker push $IMAGE_URL:${TAG}
+    docker push $IMAGE_URL:${DOCKER_TAG}
     docker push $IMAGE_URL:latest
-    echo $IMAGE_URL:${TAG} | tee -a ${ARTIFACT_DIR}/img.txt
+    echo $IMAGE_URL:${DOCKER_TAG} | tee -a ${ARTIFACT_DIR}/img.txt
+
     set +e
     docker rmi ${IMG_TMP}
-    docker rmi $IMAGE_URL:${TAG}
+    docker rmi $IMAGE_URL:${DOCKER_TAG}
     docker rmi $IMAGE_URL:latest
     set -e
   else
@@ -139,23 +170,49 @@ do_compose_test() {
   docker-compose rm -f
 }
 
-do_release_pre() {
-  if ! command -v github-release; then
-      go get github.com/github-release/github-release
+do_release() {
+  local GITHUB_USER=${GITHUB_USER:-chimeh}
+  local GITHUB_REPO=${GITHUB_REPO:-${REPO_NAME}}
+
+  CUR_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+  if [[ ! "${CUR_BRANCH_NAME}" ="release" ]];then
+    # branch name not contain `release`, error.
+    echo "branch name not contain keyword 'release', error."
+    exit 1
   fi
+  BRANCH_MARJOR_MINOR=$(echo ${CUR_BRANCH_NAME} |  perl -ne '$_ =~ /\b((0|[1-9][0-9]*).(0|[1-9][0-9]*))/;print $1' -)
+  SRC_MARJOR_MINOR=$(echo ${SRC_VERSION} |  perl -ne '$_ =~ /\b((0|[1-9][0-9]*).(0|[1-9][0-9]*))/;print $1' -)
+
+  if [[ "${BRANCH_MARJOR_MINOR}" =~ "${SRC_MARJOR_MINOR}" ]];then
+
+  else
+    echo " error. Marjor.Minor should be equal, ${BRANCH_MARJOR_MINOR} on branch name ${CUR_BRANCH_NAME} via ${SRC_MARJOR_MINOR} on src."
+    exit 1
+  fi
+  # word 'alpha' 'beta' appear on branch name or commit message, assume that release
+  if [[ "${CUR_BRANCH_NAME}" =~ "alpha" ]];then
+    PRERELEASE_TYPE='alpha'
+  elif [[ "${CUR_BRANCH_NAME}" =~ "beta" ]];then
+    PRERELEASE_TYPE='beta'
+  else
+    PRERELEASE_TYPE='alpha'
+  fi
+
   if [[ -z ${GITHUB_TOKEN} ]];then
       echo "GITHUB_TOKEN not set ,exit!"
       exit 1
   fi
-  GITHUB_USER=${GITHUB_USER:-chimeh}
-  GITHUB_REPO=${GITHUB_USER:-chimeh}
-  TAG=$(head -n 1 ${SRC_TOP}/VERSION)
+  if ! command -v github-release; then
+      echo "github-release cli not found!"
+      go get github.com/github-release/github-release
+  fi
 
+  RELEASE_TITLE="${SRC_VERSION} ${PRERELEASE_TYPE} release"
  github-release edit \
     --user ${GITHUB_USER} \
     --repo ${GITHUB_REPO} \
-    --tag ${TAG} \
-    --name "$(basename ${SRC_TOP}) ${TAG} pre release" \
+    --tag ${SRC_VERSION} \
+    --name "${RELEASE_TITLE}" \
     --description  < ${ARTIFACT_DIR}/buildnote.md
   rv=$?
   if [[ ${rv} -ne 0 ]];then
@@ -163,7 +220,7 @@ do_release_pre() {
       --user ${GITHUB_USER} \
       --repo ${GITHUB_REPO} \
       --tag ${TAG} \
-      --name "$(basename ${SRC_TOP}) ${TAG} pre release" \
+      --name "${RELEASE_TITLE}" \
       --description < ${ARTIFACT_DIR}/buildnote.md
       --pre-release
   fi
@@ -181,9 +238,6 @@ case ${ACTION} in
         export USE_PUSHED_IMG=1
         do_compose_gen
         do_compose_test
-        ;;
-    release)
-        echo "todo"
         ;;
     *)
         echo "unkown action"
