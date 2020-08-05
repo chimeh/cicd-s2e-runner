a8289af5310865c8d62adf4df128f389594764a8
Jimmy <jimminh@163.com>
2020-08-05 09:54:56 +0800

    “os/centos/mk-runner-cloud.sh”

--- os/centos/mk-runner-cloud.sh
+++ os/centos/mk-runner-cloud.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR="$(dirname "$(realpath "${THIS_SCRIPT}")")"
 
 TC_DIR=/s2erunner-src/os/centos/scripts/installers
 
-bash /s2erunner-src/os/centos/scripts/0helper-yum.sh
-bash /s2erunner-src/os/centos/scripts/0helper-basic.sh
+bash ${TC_DIR}/0helper-yum.sh
+bash ${TC_DIR}/0helper-basic.sh
 
 TCLIST=(
   c_c++.sh
